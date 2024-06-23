1129d76fa6146bad220b7d2a226451578d7e0bba
Filipe Suhett <filipe.gmx@gmail.com>
2024-06-23 01:58:50 -0300

    [staging/backend] add-searchCar-in-backend

---
+++ backend/routes/searchCarRoute.js
@@ -0,0 +1,26 @@
+const router = require('express-promise-router')();
+const basicAuth = require('express-basic-auth');
+
+const searchCar = require('../controllers/searchCar');
+
+const auth = require('../controllers/auth');
+
+// const multer = require('multer');
+// const upload = multer({ dest : 'uploads'});
+
+var challangeAuth = basicAuth( 
+    {
+        authorizer : auth.authenticate,
+        authorizeAsync : true,
+        unauthorizedResponse : { sucesso : 0, error: "usuario ou senha nao confere", cod_erro : 0 }
+    }
+);
+
+// router.get('/brand', searchCar.searchBrand);
+router.get('/brand', challangeAuth, searchCar.searchBrand);
+router.get('/model', challangeAuth, searchCar.searchModel);
+router.get('/year', challangeAuth,searchCar.searchYear)
+router.get('/Version', challangeAuth,searchCar.searchVersion)
+
+
+module.exports = router;
